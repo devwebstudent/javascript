82eb717b3b7b8585bf04e3f7d111daf2c28da56e
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2023-11-28 11:49:13 -0300

    criei ex011, ex012 e ex013

---
+++ aula12/ex013.js
@@ -0,0 +1,48 @@
+var agora = new Date()
+var diaSem = agora.getDay() //Para pegarmos o dia da semana.
+
+/* Para o JavaScript, os dias da semana são assim:
+
+0 = Domingo
+1 = Segunda
+2 = Terça
+3 = Quarta
+4 = Quinta
+5 = Sexta
+6 = Sábado
+
+*/
+
+switch (diaSem) {
+    case 0: //Se meu dia da semana for 0
+        console.log('Hoje é Domingo')
+        break
+    
+    case 1: //Se meu dia da semana for 1
+        console.log('Hoje é Segunda')
+        break
+    
+    case 2: //Se meu dia da semana for 2
+        console.log('Hoje é Terça')
+        break
+    
+    case 3: //Se meu dia da semana for 3
+        console.log('Hoje é Quarta')
+        break
+
+    case 4: //Se meu dia da semana for 4
+        console.log('Hoje é Quinta')
+        break
+    
+    case 5: //Se meu dia da semana for 5
+        console.log('Hoje é Sexta')
+        break
+
+    case 6: //Se meu dia da semana for 6
+        console.log('Hoje é Sábado')
+        break
+
+    default:
+        console.log('[ERRO]: Dia inválido!')
+        break //Esse break é opcional, pois não temos comandos em baixo
+}
