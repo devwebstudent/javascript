e9bdb9da09886012656bbb2efcdfbf5de8724713
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2023-12-04 08:36:50 -0300

    aula 14 / ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,19 @@
+/*
+--------------------------- WHILE ---------------------------
+
+var c = 1
+while (c <= 10) {
+    console.log('Olá')
+    c++
+}
+
+*/
+
+
+// ---------------------- FOR ----------------------
+
+console.log(`Vai começar`)
+for (var c = 1; c <= 10; c++) {
+    console.log(`Teste ${c}`)
+}
+console.log(`FIM!`)
